34fce9a418658094453ddaadc93d053137c807a4
TomTruyen <tom.truyen@gmail.com>
2023-04-21 20:57:43 +0200

    feat: splashscreen

--- app/src/main/java/com/tomtruyen/fitfood/MainActivity.kt
+++ app/src/main/java/com/tomtruyen/fitfood/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.tomtruyen.fitfood.ui.screens.LoginScreen
 import com.tomtruyen.fitfood.ui.theme.FitFoodTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             FitFoodTheme {
                 // A surface container using the 'background' color from the theme
