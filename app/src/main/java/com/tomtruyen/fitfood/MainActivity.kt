29b0f95daad3a5e5abb0202b25710a78022730dc
TomTruyen <tom.truyen@gmail.com>
2023-05-21 15:51:01 +0200

    fix: missing navigation on already logged in

--- app/src/main/java/com/tomtruyen/fitfood/MainActivity.kt
+++ app/src/main/java/com/tomtruyen/fitfood/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.navigation.dependency
+import com.ramcosta.composedestinations.navigation.navigate
+import com.ramcosta.composedestinations.navigation.popUpTo
+import com.tomtruyen.fitfood.managers.AuthManager
 import com.tomtruyen.fitfood.ui.screens.NavGraphs
+import com.tomtruyen.fitfood.ui.screens.destinations.HomeScreenDestination
+import com.tomtruyen.fitfood.ui.screens.destinations.LoginScreenDestination
 import com.tomtruyen.fitfood.ui.theme.FitFoodTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +29,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             FitFoodTheme {
                 val navController = rememberNavController()
+
+                LaunchedEffect(Unit) {
+                    if(AuthManager.isLoggedIn()) {
+                        navController.navigate(HomeScreenDestination) {
+                            popUpTo(LoginScreenDestination.route) {
+                                inclusive = true
+                            }
+                        }
+                    }
+                }
 
                 // A surface container using the 'background' color from the theme
                 Surface(
